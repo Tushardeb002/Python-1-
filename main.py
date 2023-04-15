8524f7c7256c1589dd473311e65e85b146094b70
Tushar Deb <106898218+Tushardeb002@users.noreply.github.com>
2023-04-15 18:55:04 +0000

    Countdown Project

--- main.py
+++ main.py
@@ -1,162 +1,91 @@
-# Python program to create a simple GUI
-# calculator using Tkinter
-
-# import everything from tkinter module
+import time
 from tkinter import *
-
-# globally declare the expression variable
-expression = ""
+from tkinter import messagebox
 
 
-# Function to update expression
-# in the text entry box
-def press(num):
-	# point out the global expression variable
-	global expression
+# creating Tk window
+root = Tk()
 
-	# concatenation of string
-	expression = expression + str(num)
+# setting geometry of tk window
+root.geometry("300x250")
 
-	# update the expression by using set method
-	equation.set(expression)
+# Using title() to display a message in
+# the dialogue box of the message in the
+# title bar.
+root.title("Time Counter")
+
+# Declaration of variables
+hour=StringVar()
+minute=StringVar()
+second=StringVar()
+
+# setting the default value as 0
+hour.set("00")
+minute.set("00")
+second.set("00")
+
+# Use of Entry class to take input from the user
+hourEntry= Entry(root, width=3, font=("Arial",18,""),
+				textvariable=hour)
+hourEntry.place(x=80,y=20)
+
+minuteEntry= Entry(root, width=3, font=("Arial",18,""),
+				textvariable=minute)
+minuteEntry.place(x=130,y=20)
+
+secondEntry= Entry(root, width=3, font=("Arial",18,""),
+				textvariable=second)
+secondEntry.place(x=180,y=20)
 
 
-# Function to evaluate the final expression
-def equalpress():
-	# Try and except statement is used
-	# for handling the errors like zero
-	# division error etc.
-
-	# Put that code inside the try block
-	# which may generate the error
+def submit():
 	try:
-
-		global expression
-
-		# eval function evaluate the expression
-		# and str function convert the result
-		# into string
-		total = str(eval(expression))
-
-		equation.set(total)
-
-		# initialize the expression variable
-		# by empty string
-		expression = ""
-
-	# if error is generate then handle
-	# by the except block
+		# the input provided by the user is
+		# stored in here :temp
+		temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
 	except:
+		print("Please input the right value")
+	while temp >-1:
+		
+		# divmod(firstvalue = temp//60, secondvalue = temp%60)
+		mins,secs = divmod(temp,60)
 
-		equation.set(" error ")
-		expression = ""
+		# Converting the input entered in mins or secs to hours,
+		# mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+		# 50min: 0sec)
+		hours=0
+		if mins >60:
+			
+			# divmod(firstvalue = temp//60, secondvalue
+			# = temp%60)
+			hours, mins = divmod(mins, 60)
+		
+		# using format () method to store the value up to
+		# two decimal places
+		hour.set("{0:2d}".format(hours))
+		minute.set("{0:2d}".format(mins))
+		second.set("{0:2d}".format(secs))
 
+		# updating the GUI window after decrementing the
+		# temp value every time
+		root.update()
+		time.sleep(1)
 
-# Function to clear the contents
-# of text entry box
-def clear():
-	global expression
-	expression = ""
-	equation.set("")
+		# when temp value = 0; then a messagebox pop's up
+		# with a message:"Time's up"
+		if (temp == 0):
+			messagebox.showinfo("Time Countdown", "Time's up ")
+		
+		# after every one sec the value of temp will be decremented
+		# by one
+		temp -= 1
 
+# button widget
+btn = Button(root, text='Set Time Countdown', bd='5',
+			command= submit)
+btn.place(x = 70,y = 120)
 
-# Driver code
-if __name__ == "__main__":
-	# create a GUI window
-	gui = Tk()
-
-	# set the background colour of GUI window
-	gui.configure(background="light green")
-
-	# set the title of GUI window
-	gui.title("Simple Calculator")
-
-	# set the configuration of GUI window
-	gui.geometry("270x150")
-
-	# StringVar() is the variable class
-	# we create an instance of this class
-	equation = StringVar()
-
-	# create the text entry box for
-	# showing the expression .
-	expression_field = Entry(gui, textvariable=equation)
-
-	# grid method is used for placing
-	# the widgets at respective positions
-	# in table like structure .
-	expression_field.grid(columnspan=4, ipadx=70)
-
-	# create a Buttons and place at a particular
-	# location inside the root window .
-	# when user press the button, the command or
-	# function affiliated to that button is executed .
-	button1 = Button(gui, text=' 1 ', fg='black', bg='red',
-					command=lambda: press(1), height=1, width=7)
-	button1.grid(row=2, column=0)
-
-	button2 = Button(gui, text=' 2 ', fg='black', bg='red',
-					command=lambda: press(2), height=1, width=7)
-	button2.grid(row=2, column=1)
-
-	button3 = Button(gui, text=' 3 ', fg='black', bg='red',
-					command=lambda: press(3), height=1, width=7)
-	button3.grid(row=2, column=2)
-
-	button4 = Button(gui, text=' 4 ', fg='black', bg='red',
-					command=lambda: press(4), height=1, width=7)
-	button4.grid(row=3, column=0)
-
-	button5 = Button(gui, text=' 5 ', fg='black', bg='red',
-					command=lambda: press(5), height=1, width=7)
-	button5.grid(row=3, column=1)
-
-	button6 = Button(gui, text=' 6 ', fg='black', bg='red',
-					command=lambda: press(6), height=1, width=7)
-	button6.grid(row=3, column=2)
-
-	button7 = Button(gui, text=' 7 ', fg='black', bg='red',
-					command=lambda: press(7), height=1, width=7)
-	button7.grid(row=4, column=0)
-
-	button8 = Button(gui, text=' 8 ', fg='black', bg='red',
-					command=lambda: press(8), height=1, width=7)
-	button8.grid(row=4, column=1)
-
-	button9 = Button(gui, text=' 9 ', fg='black', bg='red',
-					command=lambda: press(9), height=1, width=7)
-	button9.grid(row=4, column=2)
-
-	button0 = Button(gui, text=' 0 ', fg='black', bg='red',
-					command=lambda: press(0), height=1, width=7)
-	button0.grid(row=5, column=0)
-
-	plus = Button(gui, text=' + ', fg='black', bg='red',
-				command=lambda: press("+"), height=1, width=7)
-	plus.grid(row=2, column=3)
-
-	minus = Button(gui, text=' - ', fg='black', bg='red',
-				command=lambda: press("-"), height=1, width=7)
-	minus.grid(row=3, column=3)
-
-	multiply = Button(gui, text=' * ', fg='black', bg='red',
-					command=lambda: press("*"), height=1, width=7)
-	multiply.grid(row=4, column=3)
-
-	divide = Button(gui, text=' / ', fg='black', bg='red',
-					command=lambda: press("/"), height=1, width=7)
-	divide.grid(row=5, column=3)
-
-	equal = Button(gui, text=' = ', fg='black', bg='red',
-				command=equalpress, height=1, width=7)
-	equal.grid(row=5, column=2)
-
-	clear = Button(gui, text='Clear', fg='black', bg='red',
-				command=clear, height=1, width=7)
-	clear.grid(row=5, column='1')
-
-	Decimal= Button(gui, text='.', fg='black', bg='red',
-					command=lambda: press('.'), height=1, width=7)
-	Decimal.grid(row=6, column=0)
-	# start the GUI
-	gui.mainloop()
+# infinite loop which is required to
+# run tkinter program infinitely
+# until an interrupt occurs
+root.mainloop()
